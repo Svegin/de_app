2664917213ec50a4fd87a68363faf02fc1dad032
Svegin <svegin@Svegins-Mac-mini.local>
2016-09-17 13:05:51 -0500

    Initial Commit

---
+++ server/fixture.js
@@ -0,0 +1,163 @@
+if (Agency.find().count() === 0) {
+  
+  Agency.insert({
+    topic:'portfolio',
+    title: 'Branding',
+    subject: 'Graphic Design', 
+    url: '#',
+    img_thumb:'img/portfolio/brand-ge.png',
+    img_pre:'/img/portfolio/brand-ge-preview.png',
+    txt_txt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ratione accusantium, optio et eos aliquid fuga sed odio deleniti accusamus aperiam quasi ullam ea libero tempore itaque. Architecto, atque, earum.',
+  });
+
+  Agency.insert({
+    topic:'portfolio',
+    title: 'Catalog',
+    subject: 'Graphic Design', 
+    url: '#',
+    img_thumb:'img/portfolio/broshure_003.png',
+    img_pre:'/img/portfolio/broshure_003-preview.png',
+    txt_txt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ratione accusantium, optio et eos aliquid fuga sed odio deleniti accusamus aperiam quasi ullam ea libero tempore itaque. Architecto, atque, earum.',
+  });
+
+    Agency.insert({
+    topic:'portfolio',
+    title: 'Church Site',
+    subject: 'Responsive Web Design', 
+    url: '#',
+    img_thumb:'img/portfolio/jdw-400.png',
+    img_pre:'/img/portfolio/jdw-preview.png',
+    txt_txt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ratione accusantium, optio et eos aliquid fuga sed odio deleniti accusamus aperiam quasi ullam ea libero tempore itaque. Architecto, atque, earum.',
+  });
+
+Agency.insert({
+    topic:'portfolio',
+    title: 'Publishing House site',
+    subject: 'Responsive Web Design + e-commerce', 
+    url: '#',
+    img_thumb:'img/portfolio/new-old-4-400.png',
+    img_pre:'/img/portfolio/new-old-4-preview.png',
+    txt_txt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ratione accusantium, optio et eos aliquid fuga sed odio deleniti accusamus aperiam quasi ullam ea libero tempore itaque. Architecto, atque, earum.',
+  });
+
+Agency.insert({
+    topic:'portfolio',
+    title: 'Modern Art site',
+    subject: 'Responsive Web Design + e-commerce', 
+    url: '#',
+    img_thumb:'img/portfolio/svegin-400.png',
+    img_pre:'/img/portfolio/svegin-preview.png',
+    txt_txt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ratione accusantium, optio et eos aliquid fuga sed odio deleniti accusamus aperiam quasi ullam ea libero tempore itaque. Architecto, atque, earum.',
+  });
+
+Agency.insert({
+   topic:'portfolio',
+    title: 'Books',
+    subject: 'Graphic Design', 
+    url: '#',
+    img_thumb:'img/portfolio/Book-Cover-400.png',
+    img_pre:'/img/portfolio/Book-Cover-preview.png',
+    txt_txt:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ratione accusantium, optio et eos aliquid fuga sed odio deleniti accusamus aperiam quasi ullam ea libero tempore itaque. Architecto, atque, earum.',
+  });
+
+Agency.insert({
+   topic:'clients',
+    title: 'sweet home',
+    subject: 'logo', 
+    url: '#',
+    img_thumb:'img/logos/logo-01.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+Agency.insert({
+   topic:'clients',
+    title: 'studio',
+    subject: 'logo', 
+    url: '#',
+    img_thumb:'img/logos/logo-02.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+Agency.insert({
+   topic:'clients',
+    title: 'passion within',
+    subject: 'logo', 
+    url: '#',
+    img_thumb:'img/logos/logo-03.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+Agency.insert({
+   topic:'clients',
+    title: 'machete',
+    subject: 'logo', 
+    url: '#',
+    img_thumb:'img/logos/logo-04.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+Agency.insert({
+   topic:'clients',
+    title: 'machete',
+    subject: 'logo', 
+    url: '#',
+    img_thumb:'img/logos/logo-05.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+Agency.insert({
+   topic:'clients',
+    title: 'machete',
+    subject: 'logo', 
+    url: '#',
+    img_thumb:'img/logos/logo-05.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+Agency.insert({
+   topic:'clients',
+    title: 'machete',
+    subject: 'logo', 
+    url: '#',
+    img_thumb:'img/logos/logo-06.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+  
+Agency.insert({
+   topic:'teams',
+    name: 'Eugene Svezhintsev',
+    subject: 'Web developer', 
+    url: '#',
+    img_thumb:'img/team/team1.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+Agency.insert({
+   topic:'teams',
+    name: 'David Svezhintsev',
+    subject: 'Front end developer', 
+    url: '#',
+    img_thumb:'img/team/team2.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+Agency.insert({
+   topic:'teams',
+    name: 'Ilona Crosby',
+    subject: 'Marketer', 
+    url: '#',
+    img_thumb:'img/team/team3.png',
+    img_pre:'',
+    txt_txt:'',
+  });
+
+}
